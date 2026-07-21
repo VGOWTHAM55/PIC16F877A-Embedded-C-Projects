5c6dd7be06bc1628e2b61a09967f54c7c765bdb4
GOWTHAM V <vgowtham9787@gmail.com>
2026-07-21 20:51:53 +0530

    Add LED blinking functionality in main.c
    Implement LED blinking pattern using PORTB.

--- 01_GPIO_Basics/03_LED_FR_RV/main.c
+++ 01_GPIO_Basics/03_LED_FR_RV/main.c
@@ -1,1 +1,32 @@
+#include<pic.h>
 
+void delay()
+{
+	unsigned int i;
+	for(i = 0; i < 50000; i++);
+
+}
+
+void main()
+{
+	TRISB = 0X00;
+	PORTB = 0x00;
+	unsigned char i;
+
+	while(1)
+	{
+				
+		for(i = 0x01; i < 0x80; i <<= 1)
+		{
+			PORTB = i;
+			delay();
+		}
+		
+		for(i = 0x80; i >= 0x01; i >>= 1)
+		{
+    		PORTB = i;
+    		delay();
+		}
+	}
+}
+		
